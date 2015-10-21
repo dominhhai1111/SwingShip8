11f18bad936b1db24280a97b3c99142faa8e4a88
dominhhai1096 <dominhhai1096@gmail.com>
2015-10-21 19:14:37 +0700

    Initial Commit

---
+++ SwingShip8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwingShip8
+//
+//  Created by Do Minh Hai on 10/21/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwingShip8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwingShip8
+//
+//  Created by Do Minh Hai on 10/21/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
